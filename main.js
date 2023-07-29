45d7e35df6ad6758782996e90a120ec229d80345
Turbo <117492413+Pablo-f42@users.noreply.github.com>
2023-07-29 09:29:09 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
+
 function startCountdown() {
-  var minutesInput = document.getElementById('minutes');
-  var countdownDisplay = document.getElementById('countdownDisplay');
-  var minutes = parseInt(minutesInput.value);
+  let minutesInput = document.getElementById('minutes');
+  let countdownDisplay = document.getElementById('countdownDisplay');
+  let minutes = parseInt(minutesInput.value);
 
   if (isNaN(minutes) || minutes <= 0) {
     alert('Ingresa un número de minutos válido.');
     return;
   }
 
-  var millisecondsRemaining = minutes * 60 * 1000;
+  let millisecondsRemaining = minutes * 60 * 1000;
   countdownDisplay.textContent = formatTime(millisecondsRemaining);
 
-  var countdownInterval = setInterval(function() {
-    millisecondsRemaining -= 1000; // Actualizamos los segundos (1000 ms)
+  let countdownInterval = setInterval(function() {
+    millisecondsRemaining -= 1000; 
     if (millisecondsRemaining <= 0) {
       clearInterval(countdownInterval);
-      countdownDisplay.textContent = '¡Tiempo finalizado!';
+      countdownDisplay.textContent = '00:00';
     } else {
       countdownDisplay.textContent = formatTime(millisecondsRemaining);
     }
-  }, 1000); // Intervalo de 1000 ms (1 segundo)
+  }, 1000); 
 }
 
 function formatTime(milliseconds) {
-  var minutes = Math.floor(milliseconds / 60000);
-  var seconds = Math.floor((milliseconds % 60000) / 1000);
+  let minutes = Math.floor(milliseconds / 60000);
+  let seconds = Math.floor((milliseconds % 60000) / 1000);
   return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
